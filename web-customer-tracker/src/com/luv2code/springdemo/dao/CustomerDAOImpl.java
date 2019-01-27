b99686a9b143b218ae2fd9d4d43c4a9081ca2fd3
KerstingJ <Kersting.Josh@Gmail.com>
2019-01-27 18:15:44 -0500

    Added Customer DAO and Updated Controller

---
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Transactional
+	@Override
+	public List<Customer> getCustomers() {
+		
+		
+		//get Current Session
+		Session session = sessionFactory.getCurrentSession();
+		
+		//Hibernate Query
+		Query<Customer> theQuery = session.createQuery("from customer", Customer.class);
+		
+		//get the result list
+		List<Customer> customerList = theQuery.getResultList();
+		
+		
+		return customerList;
+	}
+
+}
