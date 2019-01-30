0211a3476c3eb35c0a3fb787a043e51c540016a6
KerstingJ <Kersting.Josh@Gmail.com>
2019-01-30 02:48:56 -0500

    changed logging messages

--- Spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,14 +13,14 @@ public class MyDemoLoggingAspect {
 	@Before("execution(public void addAccount())")
 	public void beforeAddAccountAdvice( ) {
 		
-		System.out.println("\n=====>>> @Before advice on addAccount()");
+		System.out.println("\n=====>>> @Before advice on addAccount() in any Class");
 		System.out.println("=====>>>\t" + getClass() + "\n");
 	}
 	
 	@After("execution(public void com.luv2code.aopdemo.dao.MemberDAO.add*())")
 	public void afterAddAdvice() {
 		
-		System.out.println("\n=====>>> @After advice on any method starting with add");
+		System.out.println("\n=====>>> @After advice on any method in MemberDAO.class starting with add");
 		System.out.println("=====>>>\t" + getClass() + "\n");
 	}
 
